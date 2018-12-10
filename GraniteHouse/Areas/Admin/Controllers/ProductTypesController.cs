4eed01c50cc3586ee630d152cd91637c397382fb
demir005 <demirmah93@gmail.com>
2018-12-10 23:14:26 +0100

    Added Ctor

--- GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
+++ GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GraniteHouse.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GraniteHouse.Areas.Admin.Controllers
@@ -9,6 +10,13 @@ namespace GraniteHouse.Areas.Admin.Controllers
     [Area("Admin")]
     public class ProductTypesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
