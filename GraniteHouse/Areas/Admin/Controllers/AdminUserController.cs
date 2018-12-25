41f4f35b981f8638b554b9c0e30463dd42a5c70b
demir005 <demirmah93@gmail.com>
2018-12-25 11:31:20 +0100

    Admin Users Added

---
+++ GraniteHouse/Areas/Admin/Controllers/AdminUserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminUserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AdminUserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.ApplicationUser.ToList());
+        }
+    }
+}
