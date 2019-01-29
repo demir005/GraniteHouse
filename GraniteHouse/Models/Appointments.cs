337789059b47370466c16295a18c3f2eb2568c0d
demir005 <demirmah93@gmail.com>
2019-01-29 21:51:21 +0100

    AppointmentVM added and Controller

--- GraniteHouse/Models/Appointments.cs
+++ GraniteHouse/Models/Appointments.cs
@@ -11,7 +11,12 @@ namespace GraniteHouse.Models
     {
         public int Id { get; set; }
 
-       
+        [Display(Name = "Sales Person")]
+        public string SalesPersonId { get; set; }
+
+        [ForeignKey("SalesPersonId")]
+        public virtual ApplicationUser SalesPerson { get; set; }
+
         public DateTime AppointmentDate { get; set; }
 
         [NotMapped]
