4d1bd6d4b312930bbf75e52fbb3c7d0bd4ab6431
demir005 <demirmah93@gmail.com>
2019-01-30 22:10:44 +0100

    Search criteria for appointment

--- GraniteHouse/Areas/Admin/Controllers/AppointmentsController.cs
+++ GraniteHouse/Areas/Admin/Controllers/AppointmentsController.cs
@@ -23,7 +23,7 @@ namespace GraniteHouse.Areas.Admin.Controllers
             _db = db;
         }
 
-        public async Task<IActionResult>  Index()
+        public async Task<IActionResult>  Index(string searchName=null, string searchEmail = null, string searchPhone=null, string searchDate = null)
         {
             System.Security.Claims.ClaimsPrincipal currentUser = this.User;
             var claimsIdentity = (ClaimsIdentity)this.User.Identity;
@@ -34,11 +34,39 @@ namespace GraniteHouse.Areas.Admin.Controllers
                 Appointments = new List<Models.Appointments>()
             };
 
+          
+
             appointmentVM.Appointments = _db.Appointments.Include(a => a.SalesPerson).ToList();
-            if(User.IsInRole(SD.AdminEndUser))
+
+
+
+
+            if (searchName != null)
             {
-                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.SalesPersonId == claim.Value).ToList();
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.CustomerName.ToLower().Contains(searchName.ToLower())).ToList();
             }
+            if (searchEmail != null)
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.CustomerEmail.ToLower().Contains(searchEmail.ToLower())).ToList();
+            }
+            if (searchPhone != null)
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.CustomerPhoneNumber.ToLower().Contains(searchPhone.ToLower())).ToList();
+            }
+            if (searchDate != null)
+            {
+                try
+                {
+                    DateTime appDate = Convert.ToDateTime(searchDate);
+                    appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.AppointmentDate.ToShortDateString().Equals(appDate.ToShortDateString())).ToList();
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+            }
+
 
             return View(appointmentVM);
         }
